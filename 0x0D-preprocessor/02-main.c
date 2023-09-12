e14a24fb7c98afdf4943dad850c6355039f20e71
MercedesSE17 <MercedesSE17@gmail.com>
2023-09-12 02:11:34 -0700

    hot fix

--- 0x0D-preprocessor/02-main.c
+++ 0x0D-preprocessor/02-main.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
-
-int main()
+/**
+ * main - print file name
+ *
+ * Return: Always 0.
+ */
+int main(void)
 {
-    printf("File name: %s\n", __FILE__);
-    return 0;
+	printf("%s\n", __FILE__);
+	return (0);
 }
