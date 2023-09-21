0b7e6d956935669577b73a885b2e0945d5e95cb8
MercedesSE17 <MercedesSE17@gmail.com>
2023-09-21 11:29:03 -0700

    0x12-singly_linked_lists

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,14 +1,12 @@
 #include "function_pointers.h"
 #include <stdio.h>
-
 /**
- * @array_iterator - executes function on array
- * @array - points to an array
- * @size - size of the array
- * @action -pointer to the function 
- * return : nothing
+ *array_iterator - executes function on array
+ *@array - points to an array
+ *@size - size of the array
+ *@action -pointer to the function 
+ *return : void
  */
-
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
