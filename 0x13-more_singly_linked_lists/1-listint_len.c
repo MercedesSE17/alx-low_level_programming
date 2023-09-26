9b81e2e75c5476b0b16b2ad8573053fdcdc695e3
MercedesSE17 <MercedesSE17@gmail.com>
2023-09-26 05:35:34 -0700

    fix

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -2,13 +2,13 @@
 #include "lists.h"
 /**
  * listint_len - returns the num of elements in linked list
- * @h: type of listint_t 
+ * @h:type of listint_t
  * Return:num of nodes
  */
 size_t listint_len(const listint_t *h)
 {
 	size_t num = 0;
-	
+
 	while (h != NULL)
 	{
 		num++;
